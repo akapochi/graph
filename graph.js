ce325e79d84352eb6ac44310270a3346596a9414
akapochi <redstar.ihsohaka@gmail.com>
2021-08-29 01:15:25 +0900

    ポケモンのデータを使用

--- graph.js
+++ graph.js
@@ -1,19 +1,58 @@
-const MONTHS = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
+// Chart.jsを用いた
+
+
+// const MONTHS = [
+//   'January',
+//   'February',
+//   'March',
+//   'April',
+//   'May',
+//   'June',
+//   'July',
+//   'August',
+//   'September',
+//   'October',
+//   'November',
+//   'December'
+// ];
+
+// const labels = time({ count: 7 });
+// const data = {
+//   labels: labels,
+//   datasets: [{
+//     label: 'My First Dataset',
+//     data: [65, 59, 80, 81, 56, 55, 40],
+//     fill: false,
+//     borderColor: 'rgb(75, 192, 192)',
+//     tension: 0.1
+//   }]
+// };
+
+// const ctx = document.getElementById('stage').getContext('2d');
+// const config = {
+//   type: 'line',
+//   data: data,
+// };
+// const myChart = new Chart(ctx, config);
+
+
+const dates = [
+  "2021/7/3",
+  "2021/7/4",
+  "2021/7/7",
+  "2021/7/7",
+  "2021/7/7",
+  "2021/7/7",
+  "2021/7/7",
+  "2021/7/7",
+  "2021/7/8",
+  "2021/7/8",
+  "2021/8/9",
+  "2021/8/10",
+  "2021/8/10"
 ];
 
-function months(config) {
+function time(config) {
   let cfg = config || {};
   let count = cfg.count || 12;
   let section = cfg.section;
@@ -21,21 +60,81 @@ function months(config) {
   let i, value;
 
   for (i = 0; i < count; ++i) {
-    value = MONTHS[Math.ceil(i) % 12];
+    value = dates[Math.ceil(i) % 12];
     values.push(value.substring(0, section));
   }
 
   return values;
 }
 
-const labels = months({ count: 7 });
+const item1 = [
+  22475,
+  22475,
+  22475,
+  22475,
+  22475,
+  22475,
+  22475,
+  22475,
+  23802,
+  26102,
+  9999,
+  47390,
+  70114
+];
+
+const item2 = [
+  53021,
+  53656,
+  53656,
+  56225,
+  56225,
+  56225,
+  56225,
+  56225,
+  64267,
+  56542,
+  44833,
+  36912,
+  38299
+];
+
+const item3 = [
+  47219,
+  47925,
+  47925,
+  47925,
+  47925,
+  47925,
+  47925,
+  47925,
+  48515,
+  47284,
+  6858,
+  57771,
+  43359
+];
+
+const labels = time({ count: dates.length });
 const data = {
   labels: labels,
   datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
+    label: 'ピカチュウ　Vmax　ブロモ',
+    data: item1,
     fill: false,
-    borderColor: 'rgb(75, 192, 192)',
+    borderColor: 'blue',
+    tension: 0.1
+  }, {
+    label: 'メイ　SR　ポケモンカード',
+    data: item2,
+    fill: false,
+    borderColor: "red",
+    tension: 0.1
+  }, {
+    label: 'ブラッキー　Vmax　SA',
+    data: item3,
+    fill: false,
+    borderColor: "green",
     tension: 0.1
   }]
 };
